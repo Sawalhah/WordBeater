0e92d79f13bfde0aa07f2b755ac110b826bab1fc
sawalhah <m.sawalhah@gmail.com>
2018-08-06 21:59:55 +0300

    add main JS file

---
+++ main.js
@@ -0,0 +1,52 @@
+//Globals
+let time = 5;
+let score = 0;
+let isPlaying;
+
+
+//DOM elements
+
+const wordText = document.querySelector("#word_text");
+const currentWord = document.querySelector("#current_word");
+const scoreDisplay = document.querySelector("#score");
+const timeLeft = document.querySelector("#time_left");
+const message = document.querySelector("#message");
+
+
+const words = [
+    'leather',
+    'quack',
+    'gleaming',
+    'cure',
+    'look',
+    'queen',
+    'care',
+    'jewel',
+    'chubby',
+    'creature',
+    'knee',
+    'avoid',
+    'blink',
+    'line',
+    'burly',
+    'tedious',
+    'suspect',
+    'wax',
+    'important',
+    'irritate',
+    'attractive',
+    'regret',
+    'pull',
+    'stranger',
+    'certain',
+    'disgusting',
+    'faint',
+    'skirt',
+    'dear',
+    'report',
+    'shoes',
+    'adjustment',
+    'slimy',
+    'obese',
+    'dynamic',
+]
